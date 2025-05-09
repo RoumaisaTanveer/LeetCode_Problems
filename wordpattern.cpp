fc35498c8051a100d78ceffb836fd69d29dd78da
RoumaisaTanveer <ahmashade13@gmail.com>
2025-05-09 08:35:50 +0500

    wordpattern.cpp

---
+++ wordpattern.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+       bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        istringstream stream(s);
+        string word;
+        int i = 0;
+    
+        while (stream >> word) {
+            if (i >= pattern.size()) return false;
+    
+            char p = pattern[i];
+    
+            if (charToWord.count(p)) {
+                if (charToWord[p] != word) return false;
+            } else {
+                if (wordToChar.count(word)) return false;
+    
+                charToWord[p] = word;
+                wordToChar[word] = p;
+            }
+            i++;
+        }
+    
+        return i == pattern.size();
+    }
+    
+    };
