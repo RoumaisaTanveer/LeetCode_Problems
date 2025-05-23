3aea8ed68a7ba64aab0c1a70edfa1ca567d16e6d
RoumaisaTanveer <ahmashade13@gmail.com>
2025-05-23 18:52:22 +0500

    sameTree

---
+++ 100_sameTree.cpp
@@ -0,0 +1,14 @@
+ #include <bits/stdc++.h>
+using namespace std;
+ bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+
+        if (p == nullptr || q == nullptr) return false;
+
+        if (p->val != q->val) return false;
+
+        
+
+int main() {
+    return 0;
+}
