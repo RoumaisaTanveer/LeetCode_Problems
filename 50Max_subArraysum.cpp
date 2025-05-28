e87338ea6d763d98d2c46927906686c19a9b16ec
RoumaisaTanveer <ahmashade13@gmail.com>
2025-05-28 18:17:10 +0500

    Kadane's Algorithm

---
+++ 50Max_subArraysum.cpp
@@ -0,0 +1,16 @@
+ #include <bits/stdc++.h>
+using namespace std;
+ int maxSubArray(vector<int>& nums) {
+        int max_sum = nums[0];
+        int current_sum = 0;
+
+        for (int num : nums) {
+            current_sum = max(num, current_sum + num);
+            max_sum = max(max_sum, current_sum);
+        }
+
+        return max_sum;
+    }
+int main() {
+    return 0;
+}
